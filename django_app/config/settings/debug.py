771a37a9001f277746c4a90d36ade0380c082579
Gihong Kim <gyong1211@gmail.com>
2017-07-05 14:21:50 +0900

    AWS RDS로 DB변경

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -3,12 +3,19 @@ from .base import *
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 WSGI_APPLICATION = 'config.wsgi.debug.application'
 
 DEBUG = True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 print('@@@@@@@@@@@@@@@@@@@@@@@@ DEBUG:', DEBUG)
 print('@@@@@@@@@@@@@@@@@@@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -3,12 +3,14 @@ from .base import *
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 WSGI_APPLICATION = 'config.wsgi.deploy.application'
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
+
+DATABASES = config_secret_deploy['django']['database']
 
 print('@@@@@@@@@@@@@@@@@@@@@@@@ DEBUG:', DEBUG)
 print('@@@@@@@@@@@@@@@@@@@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
